b09d109f921c4e7e9ffb6b0059ddce5e7ee8db60
Carlos Coimbra <50627827+vini547@users.noreply.github.com>
2025-07-08 21:50:01 -0300

    Update app_7.py

--- app_7.py
+++ app_7.py
@@ -13,7 +13,7 @@ sns.set_theme(style="ticks", rc=custom_params)
 
 
 # Função para ler os dados
-@st.cache_data(show_spinner= True, allow_output_mutation=True)
+@st.cache_data(show_spinner= True)
 def load_data(file_data):
     try:
         return pd.read_csv(file_data, sep=';')
@@ -21,7 +21,7 @@ def load_data(file_data):
         return pd.read_excel(file_data)
 
 # Função para filtrar baseado na multiseleção de categorias
-@st.cache_data(allow_output_mutation=True)
+@st.cache_data()
 def multiselect_filter(relatorio, col, selecionados):
     if 'all' in selecionados:
         return relatorio
